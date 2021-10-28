22a82d796b4cf8f2c87898bbb118b43e532b4b64
Victor Nogueira <vmnogueira.ti@gmail.com>
2021-10-27 22:00:03 -0300

    feat: user can add new transactions

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,9 +1,10 @@
-import {createGlobalStyle} from 'styled-components'
+import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
     :root {
         --red: #e52e4d;
         --blue: #5429cc;
+        --green: #33cc95;
         
         --blue-light: #6933ff;
         
@@ -51,5 +52,47 @@ export const GlobalStyle = createGlobalStyle`
     [disabled] {
         opacity: 0.6;
         cursor: not-allowed;
+    }
+
+
+    .react-modal-overlay {
+        background-color: rgba(0, 0, 0, 0.5);
+
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .react-modal-content {
+        width: 100%;
+        max-width: 576px;
+
+        background-color: var(--background);
+
+        padding: 3rem;
+
+        position: relative;
+        
+        border-radius: 0.25rem;
+    }
+
+    .react-modal-close {
+        position: absolute;
+        right: 1.5rem;
+        top: 1.5rem;
+        border: 0;
+        
+
+        transition: filter 0.2s;
+
+        &:hover {
+            filter: brightness(0.8);
+        }
     }
 `;
